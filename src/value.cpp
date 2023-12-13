ba35c0374c7dfcd6d819e81b6a43ddb1ceb64bab
Paul Jarrett <jarrett.paul.young@gmail.com>
2023-12-12 21:50:15 -0500

    Uninline Value static and conversion functions and helpers.

--- src/value.cpp
+++ src/value.cpp
@@ -6,6 +6,92 @@
 
 namespace cxxlox {
 
+Value Value::makeBool(bool b)
+{
+	Value v {.type = ValueType::Bool};
+	v.as.boolean = b;
+	return v;
+}
+
+Value Value::makeNumber(double d)
+{
+	Value v {.type = ValueType::Number};
+	v.as.number = d;
+	return v;
+}
+
+Value Value::makeString(ObjString* str)
+{
+	Value v {.type = ValueType::Obj};
+	v.as.obj = reinterpret_cast<Obj*>(str);
+	return v;
+}
+
+Value Value::makeObj(Obj* obj)
+{
+	Value v {.type = ValueType::Obj};
+	v.as.obj = obj;
+	return v;
+}
+
+Value Value::makeFunction(ObjFunction* fn)
+{
+	Value v {.type = ValueType::Obj};
+	v.as.obj = reinterpret_cast<Obj*>(fn);
+	return v;
+}
+
+Value Value::makeNative(ObjNative* fn)
+{
+	Value v {.type = ValueType::Obj};
+	v.as.obj = reinterpret_cast<Obj*>(fn);
+	return v;
+}
+
+Value Value::makeNil()
+{
+	return {.type = ValueType::Nil};
+}
+
+bool Value::toBool() const
+{
+	CL_ASSERT(type == ValueType::Bool);
+	return as.boolean;
+}
+
+double Value::toNumber() const
+{
+	CL_ASSERT(type == ValueType::Number);
+	return as.number;
+}
+
+Obj* Value::toObj() const
+{
+	CL_ASSERT(type == ValueType::Obj);
+	CL_ASSERT(as.obj);
+	return as.obj;
+}
+
+bool Value::isNil() const
+{
+	return type == ValueType::Nil;
+}
+
+bool Value::isBool() const
+{
+	return type == ValueType::Bool;
+}
+
+bool Value::isNumber() const
+{
+	return type == ValueType::Number;
+}
+
+bool Value::isObj() const
+{
+	return type == ValueType::Obj;
+}
+
 void printValue(Value v)
 {
 	switch (v.type) {
--- src/value.hpp
+++ src/value.hpp
@@ -30,84 +30,26 @@ struct Value {
 		Obj* obj;
 	} as;
 
-	[[nodiscard]] static Value makeBool(bool b)
-	{
-		Value v {.type = ValueType::Bool};
-		v.as.boolean = b;
-		return v;
-	}
+	// Creation helpers
+	[[nodiscard]] static Value makeBool(bool b);
+	[[nodiscard]] static Value makeNumber(double d);
+	[[nodiscard]] static Value makeString(ObjString* str);
+	[[nodiscard]] static Value makeObj(Obj* obj);
+	[[nodiscard]] static Value makeFunction(ObjFunction* fn);
+	[[nodiscard]] static Value makeNative(ObjNative* fn);
+	[[nodiscard]] static Value makeNil();
 
-	[[nodiscard]] static Value makeNumber(double d)
-	{
-		Value v {.type = ValueType::Number};
-		v.as.number = d;
-		return v;
-	}
+	// Runtime type conversions.  These should be checked with the associated
+	// `is*` function before use.
+	[[nodiscard]] bool toBool() const;
+	[[nodiscard]] double toNumber() const;
+	[[nodiscard]] Obj* toObj() const;
 
-	[[nodiscard]] static Value makeString(ObjString* str)
-	{
-		Value v {.type = ValueType::Obj};
-		v.as.obj = reinterpret_cast<Obj*>(str);
-		return v;
-	}
-
-	[[nodiscard]] static Value makeObj(Obj* obj)
-	{
-		Value v {.type = ValueType::Obj};
-		v.as.obj = obj;
-		return v;
-	}
-
-	[[nodiscard]] static Value makeFunction(ObjFunction* fn)
-	{
-		Value v {.type = ValueType::Obj};
-		v.as.obj = reinterpret_cast<Obj*>(fn);
-		return v;
-	}
-
-	[[nodiscard]] static Value makeNative(ObjNative* fn)
-	{
-		Value v {.type = ValueType::Obj};
-		v.as.obj = reinterpret_cast<Obj*>(fn);
-		return v;
-	}
-
-	static Value makeNil() { return {.type = ValueType::Nil}; }
-
-	// Trying explicit casts here.
-	[[nodiscard]] bool toBool() const
-	{
-		CL_ASSERT(type == ValueType::Bool);
-		return as.boolean;
-	}
-
-	[[nodiscard]] double toNumber() const
-	{
-		CL_ASSERT(type == ValueType::Number);
-		return as.number;
-	}
-
-	[[nodiscard]] Obj* toObj() const {
-		CL_ASSERT(type == ValueType::Obj);
-		CL_ASSERT(as.obj);
-		return as.obj;
-	}
-
-	[[nodiscard]] bool isNil() const {
-		return type == ValueType::Nil;
-	}
-
-	[[nodiscard]] bool isBool() const {
-		return type == ValueType::Bool;
-	}
-
-	[[nodiscard]] bool isNumber() const {
-		return type == ValueType::Number;
-	}
-
-	[[nodiscard]] bool isObj() const {
-		return type == ValueType::Obj;
-	}
+	// Runtime type checking.
+	[[nodiscard]] bool isNil() const;
+	[[nodiscard]] bool isBool() const;
+	[[nodiscard]] bool isNumber() const;
+	[[nodiscard]] bool isObj() const;
 
 	[[nodiscard]] bool operator==(const Value& rhs) const;
 };
