abe042f84f0b5bb2f59a95df574c91954f3f23e4
Paul Jarrett <jarrett.paul.young@gmail.com>
2023-12-14 22:59:08 -0500

    [scripts] Print passed/failed/omits/total in acceptance test.

--- scripts/run_acceptance_tests.py
+++ scripts/run_acceptance_tests.py
@@ -6,16 +6,19 @@ from project import build_bytecode_vm, project_root, run_program
 
 
 def main():
-    # Build the VM in both debug and release.  Release for running the samples
-    # Debug for rerunning failed samples for more information.
-    build_bytecode_vm(Config.Debug)
+    # Run all tests in release
     build_bytecode_vm(Config.Release)
+
+    fails = 0
+    passes = 0
+    omits = 0
 
     samples_dir = os.path.join(project_root(), 'samples')
     for file in glob(os.path.join(samples_dir, '*.lox')):
         expected_file = file.replace('.lox', '.expected')
         if not os.path.exists(expected_file):
             print(f"[OMIT] {file} ... no '.expected' output file")
+            omits += 1
             continue
 
         with open(expected_file, 'r') as expected_file:
@@ -29,12 +32,17 @@ def main():
 
         if passed:
             print(f'[PASS] {file}')
+            passes += 1
         else:
             print(f'[FAIL] {file}')
             for (real, expected) in merged:
                 print(f'    {real} <=> {expected}')
+            fails += 1
 
-        # If failed, rerun in debug.
+    print(f"Passed: {passes}")
+    print(f"Failed: {fails}")
+    print(f"Omits:  {omits}")
+    print(f"Total:  {passes + fails + omits}")
 
 
 if __name__ == "__main__":
