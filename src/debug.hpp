79ea33b4898535b3d1265577e4c597da706dd5d3
Paul Jarrett <jarrett.paul.young@gmail.com>
2023-12-18 23:07:22 -0500

    Remove unnecessary include from debug.hpp

--- src/debug.hpp
+++ src/debug.hpp
@@ -1,9 +1,10 @@
 #pragma once
 
-#include "chunk.hpp"
 #include "common.hpp"
 
 namespace cxxlox {
+
+struct Chunk;
 
 void disassembleChunk(const Chunk& chunk, const char* name);
 [[nodiscard]] int32_t disassembleInstruction(const Chunk& chunk, int32_t offset);
